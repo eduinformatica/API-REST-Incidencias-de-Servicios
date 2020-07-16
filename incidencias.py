6ad3e9827f58b6ed7a3c32f519d3afcd80e3e7e7
eduinformatica <dgomez.informatica@gmail.com>
2020-07-16 15:56:46 -0400

    Scripts JSON renombrado

--- incidencias.py
+++
@@ -1,8 +0,0 @@
-incidencias = [
-	{
-		"fecha": "13/07/2020 02:43:00",
-		"titulo": "Error en servidor srv1.haulmer.com",
-		"descripcion": "Servicio MySQL caido",
-		"agente": "srv1.haulmer.com"
-	}
-]
